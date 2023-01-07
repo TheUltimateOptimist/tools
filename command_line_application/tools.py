0acf4a50ea988c72aef82e304121831da1c089e8
TheUltimateOptimist <digitalConfidence@web.de>
2023-01-07 14:16:00 +0100

    added start and lofi command

--- command_line_application/tools.py
+++ command_line_application/tools.py
@@ -11,6 +11,22 @@ app = typer.Typer()
 app.add_typer(sessions.app, name="sessions")
 app.add_typer(topics.app, name="topics")
 
+def launch_lofi():
+    import webbrowser
+    webbrowser.open_new_tab("https://www.youtube.com/watch?v=jfKfPfyJRdk")
+
+@app.command()
+def lofi():
+    launch_lofi()
+
+@app.command()
+def start():
+    launch_lofi()
+    import subprocess
+    subprocess.call(["C:\\Users\\JDuec\\AppData\\Local\\Programs\\Microsoft VS Code\\bin\\code-tunnel.exe"])
+    from subcommands.topics import show
+    show()
+
 @app.command()
 def track(id: int = 0, duration: int = 1620, pause: int = 180, last: bool = False, secondlast: bool = False, thirdlast: bool = False):
     tracking_config = TrackingConfig(id, duration, pause, last, secondlast, thirdlast)
