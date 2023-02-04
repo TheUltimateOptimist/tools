79b4867e9a6410f679c988710c0a5ab33c157430
TheUltimateOptimist <digitalConfidence@web.de>
2023-02-04 11:53:53 +0100

    fixed #2 shifted  address 1.5cm down

--- invoicer/lib/src/invoicer_base.dart
+++ invoicer/lib/src/invoicer_base.dart
@@ -8,32 +8,6 @@ import 'package:invoicer/src/config.dart';
 import 'package:invoicer/src/widgets/widgets.dart';
 import 'package:invoicer/src/models/models.dart';
 
-// Future<void> createPdf({
-//   required String customerName,
-//   required String invoiceName,
-// }) async {
-//   company = Company.fromJson(loadJson(companyPath));
-//   customer = Customer.fromJson(loadJson(customerPath(customerName)));
-//   invoice = Invoice.fromJson(loadJson(invoicePath(invoiceName)));
-//   final logoBytes = File(logoPath).readAsBytesSync();
-//   logo = MemoryImage(logoBytes);
-//   final pdf = Document();
-//   pdf.addPage(MultiPage(
-//       pageFormat: PdfPageFormat.a4.copyWith(marginTop: 30, marginBottom: 30),
-//       build: (context) {
-//         return [Theme(child: LetterHead()),Container(height: 30), //Theme(child: Content()),
-//         Theme(child: Table(fractionalColumnWidths: [0.08333333, 0.4166666, 0.166666, 0.166666, 0.166666], children: [for(int i = 0; i < 100; i++) TableRow(children: [for(int i = 0; i < 5; i++) Text("some textssdf\nsdfsdf")])]))
-//         ];
-//       },
-//       header: (context) => Theme(child: Header()),
-//       footer: (context) => Theme(child: Footer())));
-//   final file = File(outputPath());
-//   await file.writeAsBytes(await pdf.save());
-// }
-
-// String loadJson(String filePath) {
-//   return File(filePath).readAsStringSync();
-// }
 Future<void> createPdf({
   required String customerName,
   required String invoiceName,
@@ -48,6 +22,7 @@ Future<void> createPdf({
       pageFormat: PdfPageFormat.a4.copyWith(marginTop: 30, marginBottom: 30),
       build: (context) {
         return [
+          Container(height: 1.5*PdfPageFormat.cm),
           LetterHead(),
           Container(height: 30),
           Text("Rechnung Nr. RE-${invoice.id}\n\n", style: theme.title),
